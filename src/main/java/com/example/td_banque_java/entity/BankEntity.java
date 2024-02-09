cc3b5cfdedd8a6cf6f431ba11422a8ca5326042a
Kylian Bardini <94058709+kbrdn1@users.noreply.github.com>
2024-02-09 11:26:41 +0100

    Update BankEntity.java

--- src/main/java/com/example/td_banque_java/entity/BankEntity.java
+++ src/main/java/com/example/td_banque_java/entity/BankEntity.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class BankEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @Column
