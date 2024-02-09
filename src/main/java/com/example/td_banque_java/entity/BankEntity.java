adb20732b1c16aa1578da51bcb91b712ec8fb390
David Gremigni <david.gremigni@gmail.com>
2024-02-09 11:36:45 +0100

    identity instead of auto

--- src/main/java/com/example/td_banque_java/entity/BankEntity.java
+++ src/main/java/com/example/td_banque_java/entity/BankEntity.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class BankEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column
